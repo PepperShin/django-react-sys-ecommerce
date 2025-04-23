8060330b8cf913c996b5773813df940e09476e2b
PepperShin <peep1996@naver.com>
2025-04-23 13:15:17 +0900

    commit

--- react-vite-sys-ecommerce/src/main.jsx
+++ react-vite-sys-ecommerce/src/main.jsx
@@ -1,9 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './route/Router.jsx'
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './route/Router.jsx';
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <RouterProvider router={router}/>
+  <RouterProvider router={router} />,
   // </StrictMode>,
-)
+);
